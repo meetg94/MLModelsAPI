a22079b65212a663428b9c9b37461b7219876771
Meet Guleria <meetguleria94@gmail.com>
2023-06-30 18:35:13 -0700

    v4

--- cancer_API/cancer_API/settings.py
+++ cancer_API/cancer_API/settings.py
@@ -23,12 +23,12 @@ load_dotenv()
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('DJANGO_KEY')
+SECRET_KEY = os.environ.get('DJANGO_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
@@ -71,7 +71,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'cancer_API.wsgi.application'
+WSGI_APPLICATION = 'cancer_API.wsgi.app'
 
 
 # Database
